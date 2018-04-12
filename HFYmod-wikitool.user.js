ab3ebb7fd22e1e10ffac9c5fef21e7dd6df0e069
sswanlake <38120033+sswanlake@users.noreply.github.com>
2018-04-12 11:05:49 -0400

    can highlight now! also adds empty series template

--- HFYmod-wikitool.user.js
+++ HFYmod-wikitool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HFYmod-wikitool
 // @namespace    http://tampermonkey.net/
-// @version      0.5.3
+// @version      0.5.4
 // @description  A tool for Reddit's r/HFY wiki Mods 
 // @author       /u/sswanlake
 // @match        *.reddit.com/r/HFY/comments/*
@@ -9,7 +9,7 @@
 // @grant        none
 // ==/UserScript==
 
-//what's new: correctly? sorts all flairs, creates series link
+//what's new: can highlight lines now! also adds empty series template
 
 (function() {
 	'use strict';
@@ -34,6 +34,7 @@
 	this.css("color", "#000000");
         this.css("margin", "auto");
         this.css("padding", "15px");
+        this.css("padding-bottom", "100px");
         this.css("border", "1px solid #888");
         this.css("width", "80%");
         this.css("overflow-y", "initial");
@@ -65,23 +66,22 @@
                     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Of those, <span id="storycount" style="color:red"></span> are stories and <span id="metacount" style="color:red"></span> are other submissions</p>
                     <hr/>
                     <h1><strong>WIKI:</strong> <a href="${baseDomain}/r/hfy/wiki/authors/${author}" target="_blank">${author}</a></h1>
-                    <div class="authorpage" style="border:1px solid gray; background:lightgray; height:200px; overflow-y:auto; overflow-x:auto;">
+                    <div class="authorpage" id="authorpage" style="border:1px solid gray; background:lightgray; height:250px; overflow-y:auto; overflow-x:auto;">
                         <p>**${author}**</p>
                         <p>&nbsp;</p>
                         <p>##**One Shots**</p>
                         <pre><span id="stories"></span></pre>
                         <br/>
                         <p>##**Series**</p>
-                        <p>####[SERIES](/r/hfy/wiki/series/SERIES)</p>
-                        <br/>
+                        <pre><span id="author-series"></span></pre>
+                        <p>&nbsp;</p>
                         <p>&amp;nbsp;</p>
                         <p>---</p>
                         <p>[All Authors](${baseDomain}/r/hfy/wiki/authors)</p>
                     </div>
                     <p>Don't forget to give editing permission to the user, send a message, and list the page on <a href="${baseDomain}/r/hfy/wiki/authors)" target="_blank">All Authors</a></p>
                     <hr/>
-                    series name: <input type="text" id="seriesname" value="" /> (hit enter to submit)
-                    <hr/>
+                    series name: <input type="text" id="seriesname" value="" /> (Hit enter to submit) <button id="unhighlight">Unhighlight</button>
                     <p>Other submissions:</p>
                     <div class="otherposts" style="border:1px solid gray; background:lightgray; height:250px; overflow-y:auto; overflow-x:auto;">
                         <pre><span id="otherposts"></span></pre>
@@ -90,9 +90,11 @@
                     <div style="border:1px solid gray; background:lightgray; overflow-x:auto;">
                     <pre>You have been added to the wiki. We created the following pages for you.
 &nbsp;
-<span id="serieslink"></span>* [${author}](<a href="${baseDomain}/r/hfy/wiki/authors/${author}" target="_blank">/r/hfy/wiki/authors/${author.toLowerCase()}</a>)\n
+<span id="message-serieslink"></span>* [${author}](<a href="${baseDomain}/r/hfy/wiki/authors/${author}" target="_blank">/r/hfy/wiki/authors/${author.toLowerCase()}</a>)\n
 You are free to edit your pages as you see fit. We strongly recommend maintaining your own pages. [Here](http://www.reddit.com/r/hfy/wiki/ref/wiki_updating) is a little guide to Wiki updating if you need it. If you start a new series please let us know so that we can create the page. If you have any questions send us a [message](http://www.reddit.com/message/compose?to=%2Fr%2FHFY).\n
 </pre>
+                    </div>
+                    <div class="endOfModal">
                     </div>
                 </div>
             </div>
@@ -126,7 +128,7 @@ You are free to edit your pages as you see fit. We strongly recommend maintainin
         var metacount = 0;
 
 	function load(after) {
-            $.getJSON(`${baseDomain}/user/${author}/submitted.json?sort=new&after=${after}`, function (data) {
+            $.getJSON(`https://www.reddit.com/user/${author}/submitted.json?sort=new&after=${after}`, function (data) { //&count=100 is no longer working?
                 var children = data.data.children;
                 var date;
                 $.each(children, function (i, post) {
@@ -134,13 +136,13 @@ You are free to edit your pages as you see fit. We strongly recommend maintainin
                         date = timeConvert(post.data.created_utc);
                         hfycount++;
                         if ((post.data.link_flair_css_class == "META") || (post.data.link_flair_css_class == "Text") || (post.data.link_flair_css_class == "Misc") || (post.data.link_flair_css_class == "Video") || (post.data.link_flair_text == "WP")){
-                            $("#otherposts").prepend( `* <a href="${post.data.url}" title="flair: ${post.data.link_flair_text},  created: ${date},  score: ${post.data.score}">` + post.data.title + `</a>\n` );
+                            $("#otherposts").prepend( `<label id="metacount">* <a href="${post.data.url}" title="flair: ${post.data.link_flair_text},  created: ${date},  score: ${post.data.score}">` + post.data.title + `</a>\n</label>` );
                             metacount++;
                         } else {
                             if (post.data.over_18) {
-                                $("#stories").prepend( `* [<a href="${post.data.url}" title="flair: ${post.data.link_flair_text},  created: ${date},  score: ${post.data.score}">` + (post.data.title).replace(`[OC]`, '').replace(`[PI]`, '').trim() + `</a>](` + post.data.url + `) <emphasis style="color:red;">*NSFW*</emphasis>\n` );
+                                $("#stories").prepend( `<label id="storycount">* [<a href="${post.data.url}" title="flair: ${post.data.link_flair_text},  created: ${date},  score: ${post.data.score}">` + (post.data.title).replace(`[OC]`, '').replace(`(OC)`, '').replace(`[PI]`, '').trim() + `</a>](` + post.data.url + `) <emphasis style="color:red;">*NSFW*</emphasis>\n</label>` );
                             } else {
-                                $("#stories").prepend( `* [<a href="${post.data.url}" title="flair: ${post.data.link_flair_text},  created: ${date},  score: ${post.data.score}">` + (post.data.title).replace(`[OC]`, '').replace(`[PI]`, '').trim() + `</a>](` + post.data.url + `)\n` );
+                                $("#stories").prepend( `<label id="storycount">* [<a href="${post.data.url}" title="flair: ${post.data.link_flair_text},  created: ${date},  score: ${post.data.score}">` + (post.data.title).replace(`[OC]`, '').replace(`(OC)`, '').replace(`[PI]`, '').trim() + `</a>](` + post.data.url + `)\n</label>` );
                             }
                             storycount++;
                         }
@@ -156,25 +158,58 @@ You are free to edit your pages as you see fit. We strongly recommend maintainin
                     load(lastID);
                 }
             })
-            .error(function() { $("#stories").append( `ERROR ... Shadowbanned?`); });
+            .error(function() {
+                if (author == "[deleted]") {
+                    $("#stories").append( `<span style="color:red">ERROR - ACCOUNT DELETED</span>`);
+                } else {
+                    $("#stories").append( `<span style="color:red">ERROR ... Shadowbanned?</span>`);
+                }
+            }); //end error
         } //end load
 
         load(lastID);
+        if ($("#otherposts")[0].innerHTML === "") { //hide otherposts if none present
+            $(".otherposts").css("height", "auto");
+        }
 
         // When user enters the series
         var series;
-        var seriescount;
+        var seriescount = 0;
+        var seriesContent = (`
+            <hr/>
+            <p>Series no. <span id="series-count">${seriescount}</span></p>
+            <div class="seriespage" style="border:1px solid gray; background:lightgray;">
+                <p>[**${author}**](/r/hfy/wiki/authors/${author})</p>
+                <p>&nbsp;</p>
+                <p>##**<span id="series-header">Series</span>**</p>
+                <pre><span id="series"></span></pre>
+                <p>&nbsp;</p>
+                <p>&amp;nbsp;</p>
+                <p>---</p>
+                <p>[All Series](https://www.reddit.com/r/HFY/wiki/series)</p>
+            </div>
+        `);
+
         $("#seriesname").keyup(function(e){
             var code = e.which; // recommended to use e.which, it's normalized across browsers
             if(code==13)e.preventDefault();
             if(code==13){ //13=enter
-                series = $(this).val();
+                series = $(this).val(); 
                 $(this).val(''); //reset input field
-                $("#series").append( `####[<a href="https://www.reddit.com/r/hfy/wiki/series/${series.replace(/\s/g, '_').replace(/['!"#$%&\\'()\*+,\.\/:;<=>?@\[\\\]\^`{|}~']/g,"").toLowerCase()}" target="_blank">${series}</a>](/r/hfy/wiki/series/${series.replace(/\s/g, '_').replace(/[.,\/#!?$%\^&\*;:{}=\`~()]/g,"").toLowerCase()})\n <span id="seriesStories"></span>\n` );
-                $("#serieslink").append( `* [${series}](<a href="https://www.reddit.com/r/hfy/wiki/series/${series.replace(/\s/g, '_').replace(/['!"#$%&\\'()\*+,\.\/:;<=>?@\[\\\]\^`{|}~']/g,"").toLowerCase()}" target="_blank">/r/hfy/wiki/series/${series.replace(/\s/g, '_').replace(/[.,\/#!?$%\^&\*;:{}=\`~()]/g,"").toLowerCase()}</a>)\n <span id="seriesStories"></span>\n` );
                 seriescount++;
+                $(".endOfModal").append(seriesContent);
+                $("#series-count").html(`${seriescount}`);
+                $("#author-series").append( `####[<a href="https://www.reddit.com/r/hfy/wiki/series/${series.replace(/\s/g, '_').replace(/['!"#$%&\\'()\*+,\.\/:;<=>?@\[\\\]\^`{|}~']/g,"").toLowerCase()}" target="_blank">${series}</a>](/r/hfy/wiki/series/${series.replace(/\s/g, '_').replace(/[.,\/#!?$%\^&\*;:{}=\`~()]/g,"").toLowerCase()})\n <span id="seriesStories"></span>\n` );
+                $("#message-serieslink").append( `* [${series}](<a href="https://www.reddit.com/r/hfy/wiki/series/${series.replace(/\s/g, '_').replace(/['!"#$%&\\'()\*+,\.\/:;<=>?@\[\\\]\^`{|}~']/g,"").toLowerCase()}" target="_blank">/r/hfy/wiki/series/${series.replace(/\s/g, '_').replace(/[.,\/#!?$%\^&\*;:{}=\`~()]/g,"").toLowerCase()}</a>)\n <span id="seriesStories"></span>\n` );
+                $("#series-header").html(`${series}`);
+                $("label:contains('" + series + "')").css("background-color", "yellow");
             }
-        }); //end get series name
+
+        // When the user clicks the button, unhighlight the awkward series maker
+        $("#unhighlight").click(function() {
+	        $("label").css("background-color","transparent");
+	    });
+	}); //end get series
 
     });//document ready
 
