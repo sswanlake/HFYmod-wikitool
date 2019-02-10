e3c1039692788b0a96772960a59a35981c789086
sswanlake <38120033+sswanlake@users.noreply.github.com>
2019-02-10 13:03:07 -0500

    no mod-removed, select text on click
    escapes underscores in usernames, displays "exists"/number on button, length of posts, doesn't show mod-removed, select text on click!

--- HFYmod-wikitool.user.js
+++ HFYmod-wikitool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HFYmod-wikitool
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  A tool for Reddit's r/HFY wiki Mods
 // @author       /u/sswanlake
 // @match        *.reddit.com/r/HFY/comments/*
